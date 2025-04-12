5ad52d76a2dba995577b35f02ab8cd5ad0812104
Sumit Grover <sumitgrover108@gmail.com>
2025-04-12 19:38:19 +0530

    gateway secret fix

--- src/route.ts
+++ src/route.ts
@@ -1,9 +1,12 @@
 import { Application } from 'express';
 import { healthRoutes } from '@chat/routes/health';
 import { messageRoutes } from '@chat/routes/message';
-import { verifyGatewayRequest } from '@sumaniac28/gigglobal-helper-v1';
+import { setJwtSecret, verifyGatewayRequest } from '@sumaniac28/gigglobal-helper-v1';
+import { config } from '@chat/config';
 
 const BASE_PATH = '/api/v1/message';
+
+setJwtSecret(config.GATEWAY_JWT_TOKEN!);
 
 const appRoutes = (app: Application): void => {
   app.use('', healthRoutes());
